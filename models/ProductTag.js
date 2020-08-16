6250eb900b45cd6b8a8db06d1a425921c39f4c91
ryannekillian <ryanne.killian@gmail.com>
2020-08-16 18:58:34 -0500

    got port to listen

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,47 +1,40 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
-const { Product } = require('.');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    product_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        // This is a reference to another model
-        model:'Product',
-   
-        // This is the column name of the referenced model
-        key: 'id'
-      },
-      tag_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          // This is a reference to another model
-          model: 'Tag',
-     
-          // This is the column name of the referenced model
-          key: 'id'
-        }
-      }
-    }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+	{
+		// define columns
+		id         : {
+			type          : DataTypes.INTEGER,
+			allowNull     : false,
+			primaryKey    : true,
+			autoIncrement : true
+		},
+		product_id : {
+			type       : DataTypes.INTEGER,
+			references : {
+				model : 'product',
+				key   : 'id'
+			},
+			tag_id     : {
+				type       : DataTypes.INTEGER,
+				references : {
+					model : 'tag',
+					key   : 'id'
+				}
+			}
+		}
+	},
+	{
+		sequelize,
+		timestamps      : false,
+		freezeTableName : true,
+		underscored     : true,
+		modelName       : 'product_tag'
+	}
 );
 
 module.exports = ProductTag;
